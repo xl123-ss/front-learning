5375a3871fec31bd0aa04c002670a5878579a833
xl123-ss <3179716692@qq.com>
2023-03-10 09:49:39 +0800

    vue学习

---
+++ t01vue-learning/main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product:'袜子',
+            image:'./assets/images/green.png',
+            url:'http://www.baidu.com',
+            productName:  "绿色帽子",
+            inStock:false
+        }
+    }
+})
